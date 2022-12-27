ef027f915b9f934f4b19cf5dd9362ab1ab63125a
flo7842 <florianbracq42@gmail.com>
2022-12-27 09:15:34 +0100

    Create Db connexion

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- open_food_api/open_food_api/urls.py
+++ open_food_api/open_food_api/urls.py
@@ -1,4 +1,4 @@
-"""open_food_fact URL Configuration
+"""open_food_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
